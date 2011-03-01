9d5c9169a8bf9eff0771bad0fc203f0348c4c575
KO Myung-Hun <komh@chollian.net>
2011-03-01 09:26:09 +0000

    Fix a month calculation

--- log.c
+++ log.c
@@ -36,7 +36,7 @@ void log_msg( const char *format, ... )
     tm = *localtime( &t );
 
     fprintf( fp, "%04d-%02d-%02d %02d:%02d:%02d ",
-             tm.tm_year + 1900, tm.tm_mon, tm.tm_mday,
+             tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
              tm.tm_hour, tm.tm_min, tm.tm_sec );
 
     va_start( arg, format );
